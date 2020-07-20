c2717e4267d94df7915f3b45a321bcd08582715c
Silas S. Brown <ssb22@cam.ac.uk>
2020-07-20 17:03:19 +0100

    where to find history: also cam gitlab

--- modeline.py
+++ modeline.py
@@ -27,3 +27,4 @@ vres, vres+btm_margin, vres+btm_margin+vsync_len, vres+btm_margin+vsync_len+top_
 # on GitHub at https://github.com/ssb22/config
 # and on GitLab at https://gitlab.com/ssb22/config
 # and on BitBucket https://bitbucket.org/ssb22/config
+# and at https://gitlab.developers.cam.ac.uk/ssb22/config
