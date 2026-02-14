fa8efb922dc5673114fa611bb3039045b7f60137
Silas S. Brown <ssb22@cam.ac.uk>
2026-02-14 13:12:08 +0000

    fetch filladapt if not installed

--- .xemacs/print.el
+++ .xemacs/print.el
@@ -24,9 +24,20 @@
 (defun make-region-printable ()
   (interactive)
   (setq fill-column 50)
+  (let ((inhibit-message t)) (package-install 'filladapt))
+  (require 'filladapt)
   (turn-on-filladapt-mode)
   (fill-region (region-beginning) (region-end))
   )
+(defun print-region-to-pdf ()
+  ;; Does not require lpr setup.  This one uses 20 (could change to 16, 18 etc as appropriate); long lines wrap w/out word break
+  (interactive)
+  (shell-command-on-region (region-beginning) (region-end) "enscript -p - -f Courier@20 -B -b --margins=72:72:72:72 /dev/stdin | ps2pdf - /tmp/output.pdf")
+  (message "PDF written to /tmp/output.pdf"))
+(defun print-buffer-to-pdf ()
+  (interactive)
+  (shell-command-on-region (buffer-end 0) (buffer-end 1) "enscript -p - -f Courier@20 -B -b --margins=72:72:72:72 /dev/stdin | ps2pdf - /tmp/output.pdf")
+  (message "PDF written to /tmp/output.pdf"))
 
 (condition-case nil (progn
 (add-menu-button '("File") "Remember M-x " "Pretty-Print")
